781824a47338c2faffe5fd472f61bd339c94bfec
Cristian Albu <albucristian95p@gmail.com>
2023-04-10 07:04:26 +0300

    p1 euler

---
+++ euler/even-fibonacci-numbers/euler2.test.js
@@ -0,0 +1,39 @@
+const fiboEvenSum = require("./euler2");
+
+describe("fiboEvenSum", () => {
+  test("returns a number when input is 10", () => {
+    expect(typeof fiboEvenSum(10)).toBe("number");
+  });
+
+  test("returns an even value", () => {
+    expect(fiboEvenSum(10) % 2).toBe(0);
+  });
+
+  test("sums the even-valued Fibonacci numbers when input is 8", () => {
+    expect(fiboEvenSum(8)).toBe(10);
+  });
+
+  test("sums the even-valued Fibonacci numbers when input is 10", () => {
+    expect(fiboEvenSum(10)).toBe(10);
+  });
+
+  test("sums the even-valued Fibonacci numbers when input is 34", () => {
+    expect(fiboEvenSum(34)).toBe(44);
+  });
+
+  test("sums the even-valued Fibonacci numbers when input is 60", () => {
+    expect(fiboEvenSum(60)).toBe(44);
+  });
+
+  test("sums the even-valued Fibonacci numbers when input is 1000", () => {
+    expect(fiboEvenSum(1000)).toBe(798);
+  });
+
+  test("sums the even-valued Fibonacci numbers when input is 100000", () => {
+    expect(fiboEvenSum(100000)).toBe(60696);
+  });
+
+  test("sums the even-valued Fibonacci numbers when input is 4000000", () => {
+    expect(fiboEvenSum(4000000)).toBe(4613732);
+  });
+});
---
+++ euler/multiples-of-3-and-5/euler1.test.js
@@ -0,0 +1,23 @@
+const multiplesOf3and5 = require("./euler1");
+
+describe("multiplesOf3and5", () => {
+  test("return value of 10 should be a number", () => {
+    expect(typeof multiplesOf3and5()).toBe("number");
+  });
+
+  test("returns 543 when input is 49", () => {
+    expect(multiplesOf3and5(49)).toBe(543);
+  });
+
+  test("returns 233168 when input is 1000", () => {
+    expect(multiplesOf3and5(1000)).toBe(233168);
+  });
+
+  test("returns 16687353 when input is 8456", () => {
+    expect(multiplesOf3and5(8456)).toBe(16687353);
+  });
+
+  test("returns 89301183 when input is 19564", () => {
+    expect(multiplesOf3and5(19564)).toBe(89301183);
+  });
+});
