242c763bff308d04feae137e9b62cc0ea51d23cc
Cristian Albu <albucristian95p@gmail.com>
2023-04-14 16:46:41 +0300

    merge sort

--- ALGORITHMS/merge-sort/merge-sort.test.js
+++ ALGORITHMS/merge-sort/merge-sort.test.js
@@ -1,21 +1,21 @@
-const quickSort = require("./quick-sort");
+const mergeSort = require("./merge-sort");
 
-describe("quickSort", () => {
+describe("mergeSort", () => {
   test("should sort an array of numbers in ascending order", () => {
     const unsortedArray = [5, 2, 9, 1, 5];
-    const sortedArray = quickSort(unsortedArray);
+    const sortedArray = mergeSort(unsortedArray);
     expect(sortedArray).toEqual([1, 2, 5, 5, 9]);
   });
 
   test("should return an empty array if an empty array is passed in", () => {
     const unsortedArray = [];
-    const sortedArray = quickSort(unsortedArray);
+    const sortedArray = mergeSort(unsortedArray);
     expect(sortedArray).toEqual([]);
   });
 
   test("should not mutate the original array", () => {
     const unsortedArray = [4, 3, 2, 1];
-    const sortedArray = quickSort(unsortedArray);
+    const sortedArray = mergeSort(unsortedArray);
     expect(sortedArray).toEqual([1, 2, 3, 4]);
     expect(unsortedArray).toEqual([4, 3, 2, 1]);
   });
